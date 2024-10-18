da879fd3a56313ea0e74da078e374eea246791e4
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-10-18 22:08:45 +0200

    Aggiornamento
    Presenti molti bug nella pagina da sistemare.

--- STATIC/script.js
+++ STATIC/script.js
@@ -1,42 +1,33 @@
 function RandomNumber(min, max){ //Algoritmo generazione numero random
     let casualNumber = Math.random();
-    casualNumber = casualNumber * (max - min) + min;
+    casualNumber = casualNumber  * (max - min) + min;
     casualNumber = parseInt(casualNumber);
-    console.log("test random number: ", casualNumber);
     return casualNumber;
     
 } 
-let numero_generato = RandomNumber(1, 100);
+let numero_generato = RandomNumber(1, 100); 
+console.log("Numero generato: ", numero_generato);
 
 
 
 
 
-//function inpututente(){
-   // let num_utente = document.querySelector("#userGuess").value;
-   // num_utente = parseInt(num_utente);
-    //console.log("input utente: ", num_utente);
-    //console.log(typeof num_utente)
-//} FUNZIONE DI TEST
-
-function inpututente(num_utente, numero_generato){
-    let num_utente = document.querySelector("#userGuess").value;
+function inpututente(){
+    let num_utente = document.querySelector("#invia").value;
     num_utente = parseInt(num_utente);
-console.log("Input Utente:", num_utente);
+    console.log("Input Utente:", num_utente);
 if (num_utente === numero_generato){
     console.log("Hai vinto")
 } else if  (num_utente < numero_generato){
     console.log("Il numero è troppo basso")
     } else {
-        console.log("Il numero è troppo alto")
+        console.log("Il numero è troppo alto") 
         }
         
-
-
-
-
-
 }
+
+document.querySelector("#Send").addEventListener("click", inpututente);
+
 
 
 
