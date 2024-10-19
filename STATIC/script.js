97ce6e07e0dda3ffae7632735896762aad2f7812
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-10-19 17:45:50 +0200

    Aggiornamento 3.0
    Il tutto funziona correttamente, la comunicazione con il codice avviene correttamente tra tutti gli elementi, effettuato un bug fixing di tutto il codice..

--- STATIC/script.js
+++ STATIC/script.js
@@ -7,26 +7,39 @@ function RandomNumber(min, max){ //Algoritmo generazione numero random
 } 
 let numero_generato = RandomNumber(1, 100); 
 console.log("Numero generato: ", numero_generato);
-
-
-
-
+let tentativi = 5;
 
 function inpututente(){
-    let num_utente = document.querySelector("#invia").value;
+    let num_utente = document.querySelector("#numero").value;
     num_utente = parseInt(num_utente);
+    if (isNaN(num_utente)) {
+        console.log("Errore inserire un numero valido!")
+        return;
+
+    }
     console.log("Input Utente:", num_utente);
 if (num_utente === numero_generato){
     console.log("Hai vinto")
 } else if  (num_utente < numero_generato){
-    console.log("Il numero è troppo basso")
+    tentativi = tentativi-1;
+    console.log("Il numero è troppo basso");
+    console.log("Tentativi rimanenti:",tentativi);
+    if(tentativi === 0){
+        console.log("Hai perso");
+        invia.disabled = true;
+    }
     } else {
-        console.log("Il numero è troppo alto") 
+        console.log("Il numero è troppo alto");
+        tentativi = tentativi-1;
+        console.log("Tentativi rimanenti:",tentativi)
         }
         
 }
 
-document.querySelector("#Send").addEventListener("click", inpututente);
+
+let invia = document.querySelector("#invia");
+invia.addEventListener("click", inpututente); 
+
 
 
 
